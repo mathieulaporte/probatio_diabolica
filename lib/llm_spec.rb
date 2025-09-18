7192b8cb8c141e14e0ea68460751db22a58da1cb
Mathieu Laporte <mlaporte@mesvaccins.net>
2025-09-18 15:47:43 +0200

    fix source code helper

--- lib/llm_spec.rb
+++ lib/llm_spec.rb
@@ -132,7 +132,7 @@ module LlmSpec
     end
 
     def source_code(class_or_method)
-      def extract_from_node(node, method_name, code)
+      def extract_method_from_node(node, method_name, code)
         return nil unless node.respond_to?(:child_nodes)
 
         node.child_nodes.each do |child|
@@ -140,21 +140,43 @@ module LlmSpec
             return code[child.location.start_offset...child.location.end_offset]
           end
 
-          found = extract_from_node(child, method_name, code)
+          found = extract_method_from_node(child, method_name, code)
           return found if found
         end
 
         nil
       end
+
+      def extract_class_from_node(node, class_name, code)
+        return nil unless node.respond_to?(:child_nodes)
+
+        node.child_nodes.each do |child|
+          if child.is_a?(Prism::ClassNode)
+            path = child.constant_path&.slice
+            return code[child.location.start_offset...child.location.end_offset] if path == class_name.to_s.split('::').last
+          end
+
+          found = extract_class_from_node(child, class_name, code)
+          return found if found
+        end
+
+        nil
+      end
+
       if class_or_method.is_a?(Class)
-        File.read(Object.const_source_location(class_or_method.to_s).first)
+        file, line = Object.const_source_location(class_or_method.to_s)
+        return nil unless file
+
+        code = File.read(file)
+        tree = Prism.parse(code)
+        extract_class_from_node(tree.value, class_or_method.to_s, code)
       else
         file, line = class_or_method.source_location
         return nil unless file && line
 
         code = File.read(file)
         tree = Prism.parse(code)
-        extract_from_node(tree.value, class_or_method.name, code)
+        extract_method_from_node(tree.value, class_or_method.name, code)
       end
     end
 
